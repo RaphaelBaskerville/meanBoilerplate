bfdec1c2e3d9613c138647484e05e655b3ff4264
Raphael Baskerville <raphael.baskerville@gmail.com>
2016-01-18 21:08:11 -0800

    lints this file

--- public/bookstore.js
+++ public/bookstore.js
@@ -1,12 +1,12 @@
 angular.module('bookstore', ['ui.router', 'cart', 'books', 'admin'])
-.config(function ($urlRouterProvider, $stateProvider) {
+.config(function($urlRouterProvider, $stateProvider) {
   $urlRouterProvider
   .otherwise('/signin');
 
   $stateProvider
   .state('signin', {
-      url: '/signin',
-      templateUrl: '/views/signin.html',
+    url: '/signin',
+    templateUrl: '/views/signin.html',
   })
   .state('store', {
     url: '/mainstore',
@@ -14,33 +14,38 @@ angular.module('bookstore', ['ui.router', 'cart', 'books', 'admin'])
   })
   .state('admin', {
     url: '/admin',
-    templateUrl: '/views/admin.html'
-  })
+    templateUrl: '/views/admin.html',
+  });
 })
 
-.controller('BookStoreController', function ($scope, $http, $location) {
+.controller('BookStoreController', function($scope, $http, $location) {
 
-  $http.get('/data').then(function(books){
-    console.log(books)
+  $http.get('/data').then(function(books) {
+    console.log(books);
     $scope.books = books.data;
   });
-  $scope.home = function () {
-    console.log('go home')
+
+  $scope.home = function() {
+    console.log('go home');
     $location.path('/');
-  }
-  $scope.signup = function (user) {
-    console.log('signUPUPUP user: ',user);
+  };
+
+  $scope.signup = function(user) {
+    console.log('signUPUPUP user: ', user);
+
     // post book to the db
-    return $http.post('/user', user).success(function (data,status){
-      console.log('user post success',data,status);
+    return $http.post('/user', user).success(function(data, status) {
+      console.log('user post success', data, status);
     });
-  }
-  $scope.signin = function (user) {
-    console.log('signINININ user: ',user);
+  };
+
+  $scope.signin = function(user) {
+    console.log('signINININ user: ', user);
+
     // post book to the db
-    return $http.post('/auth', user).then(function (data,status){
-      console.log('user get success',data,status);
-      // console.log('data',data.data[0].username === 'admin')
+    return $http.post('/auth', user).then(function(data, status) {
+      console.log('user get success', data, status);
+
       if (data.data.length) {
         if (data.data[0].username === 'admin') {
           $location.path('/admin');
@@ -51,70 +56,64 @@ angular.module('bookstore', ['ui.router', 'cart', 'books', 'admin'])
         }
 
       } else {
-        console.log('not logged in')
+        console.log('not logged in');
       }
     });
-  }
+  };
 
-  $scope.addBook = function (book) {
+  $scope.addBook = function(book) {
     console.log(this.cart.findBook(book.title));
     if (!this.cart.findBook(book.title).length && book.quantity > 0) {
       this.cart.books.push(book);
       book.count = 1;
       book.quantity--;
-    } else if (book.quantity){
+    } else if (book.quantity) {
       book.count++;
       book.quantity--;
 
     }
+
     if (book.quantity) {
       this.cart.total += book.price;
 
     }
   };
-  $scope.toggleCart = function () {
+
+  $scope.toggleCart = function() {
     if (!this.showCart) {
-      console.log('on',this.showCart);
+      console.log('on', this.showCart);
       this.showCart = true;
     } else {
-      console.log('off',this.showCart);
+      console.log('off', this.showCart);
       this.showCart = false;
     }
   };
+
   $scope.cart = {
     total: 0,
     books:[],
-    empty: function (x) {
+    empty: function(x) {
       this.total = 0;
       this.books = [];
       if (x) {
-        alert('your books are on the way')
+        alert('your books are on the way');
       }
     },
-    removeBook: function (index) {
+
+    removeBook: function(index) {
       var book = this.books[index];
       this.total -= book.price;
       book.count--;
       book.quantity++;
       if (book.count === 0) {
-        this.books.splice(index,1);
+        this.books.splice(index, 1);
       }
     },
-    findBook: function (search) {
-      return this.books.filter(function(book){
+
+    findBook: function(search) {
+      return this.books.filter(function(book) {
         return (book.title === search);
       });
-    }
-
+    },
   };
-})
-
-
-
-
-
-// .factory('bookFactory', [function(){
-//   return {
-
-//   }
-// }])
+});
